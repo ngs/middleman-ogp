3225c0930c9efb39549d02b44756529dacee7af9
Maarten de Waard <maarten@greenhost.nl>
2018-02-19 15:49:59 +0100

    Add author, modified_time and expiration_time to og tags
    This commit adds author type (as a profile with first_name, last_name, username
    and gender fields) and modified_time and expiration_time as properties that the
    user can set in their blog post's front matter. If set, the plugin will generate
    og: tags for each of these.
    
    I had to add the timezone to env.rb so the the tests always come out the same,
    regardless of where you run them.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,6 @@
 PROJECT_ROOT_PATH = File.dirname(File.dirname(File.dirname(__FILE__)))
 ENV['TEST'] = 'true'
+ENV['TZ'] = 'UTC'
 require "middleman-core"
 require "middleman-blog"
 require "middleman-core/step_definitions"
